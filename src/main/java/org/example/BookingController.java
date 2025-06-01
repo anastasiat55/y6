12ad682784dab04c1717fb0ae947f4107867ab3a
anastasiat55 <115346412+anastasiat55@users.noreply.github.com>
2025-06-01 19:39:03 +0300

    Update BookingController.java

--- src/main/java/org/example/BookingController.java
+++ src/main/java/org/example/BookingController.java
@@ -1,2 +1,35 @@
-package org.example;public class BookingController {
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BookingController {
+
+    @Autowired
+    private BookingService bookingService;
+
+    @GetMapping("/bookings")
+    public String bookingsPage(Model model) {
+        model.addAttribute("bookings", bookingService.getAllBookings());
+        model.addAttribute("newBooking", new Booking());
+        return "bookings";
+    }
+
+    @PostMapping("/bookings/add")
+    public String addBooking(@ModelAttribute("newBooking") Booking booking) {
+        bookingService.saveBooking(booking);
+        return "redirect:/bookings";
+    }
+
+    @PostMapping("/bookings/delete")
+    public String deleteBooking(@RequestParam("id") Long id) {
+        bookingService.deleteBooking(id);
+        return "redirect:/bookings";
+    }
 }
