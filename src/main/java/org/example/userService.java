606646dff9046de2ce91cbea75f9e34f1b530d73
anastasiat55 <115346412+anastasiat55@users.noreply.github.com>
2025-06-01 19:35:37 +0300

    Update userService.java

--- src/main/java/org/example/userService.java
+++ src/main/java/org/example/userService.java
@@ -1,2 +1,26 @@
-package org.example;public class UserService {
+package org.example;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class userService {
+
+    private List<User> users = new ArrayList<>();
+
+    public List<User> getAllUsers() {
+        return users;
+    }
+
+    public void saveUser(User user) {
+        // Проста логіка з додаванням id, якщо потрібно
+        user.setId((long) (users.size() + 1));
+        users.add(user);
+    }
+
+    public void deleteUser(Long id) {
+        users.removeIf(u -> u.getId().equals(id));
+    }
 }
