22c2855ac5a50be76ee4e16f13ba7816bd7a0e64
anastasiat55 <115346412+anastasiat55@users.noreply.github.com>
2025-06-01 19:37:15 +0300

    Update Booking.java

--- src/main/java/org/example/Booking.java
+++ src/main/java/org/example/Booking.java
@@ -1,2 +1,27 @@
-package org.example;public class Booking {
+package org.example;
+
+import java.time.LocalDate;
+
+public class Booking {
+    private Long id;
+    private Long userId;
+    private LocalDate date;
+    private String details;
+
+    public Booking() {}
+    public Booking(Long id, Long userId, LocalDate date, String details) {
+        this.id = id; this.userId = userId; this.date = date; this.details = details;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
+
+    public String getDetails() { return details; }
+    public void setDetails(String details) { this.details = details; }
 }
